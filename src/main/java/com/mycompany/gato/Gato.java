2cd9a99c7858f8b6109029a1bc450b869971c3f3
francisco <francisco.carmonao.grupoe@gmail.com>
2021-04-14 21:15:24 -0500

    ya se imprime la Matriz

--- src/main/java/com/mycompany/gato/Gato.java
+++ src/main/java/com/mycompany/gato/Gato.java
@@ -12,11 +12,9 @@ import java.util.Scanner;
 public class Gato {
     public static void main(String[] args){
         Scanner mValor=new Scanner(System.in);
-        String mPlayer1, mPlayer2, mGato;
-        int mAux;
+        String mPlayer1, mPlayer2, mPos1, mPos2;
+        int mAux, mVer, mHor;
         String[][] mArreglo =  new String[3][3];
-        
-        mArreglo[1][1]="-";
         
         System.out.println("*************************************");
         System.out.println("   Bienvenidos al juego del gato!!   ");
@@ -30,17 +28,38 @@ public class Gato {
         for(int i=1; i<=9; i++){
             mAux=i%2;
             if(mAux==0){
-                System.out.println("Turno de "+ mPlayer1);
-                mGato=mValor.nextLine();
-                mArreglo[1][1]=mGato;
-                System.out.println(mArreglo[1][1]);
-            }else{
                 System.out.println("Turno de "+ mPlayer2);
-                mGato=mValor.nextLine();
-                mArreglo[2][2]=mGato;
-                System.out.println(mArreglo[2][2]);
+                System.out.println("Ingrese posicion vertical");
+                mPos1 = mValor.nextLine();
+                System.out.println("Ingrese posicion vertical");
+                mPos2 = mValor.nextLine();
+                
+                mVer=Integer.parseInt(mPos1);
+                mHor=Integer.parseInt(mPos2);
+                
+                mArreglo[mHor][mVer]= " O ";
+                
+            }else{
+                System.out.println("Turno de "+ mPlayer1);
+                System.out.println("Ingrese posicion vertical");
+                mPos1 = mValor.nextLine();
+                System.out.println("Ingrese posicion vertical");
+                mPos2 = mValor.nextLine();
+                
+                mVer=Integer.parseInt(mPos1);
+                mHor=Integer.parseInt(mPos2);
+                
+                mArreglo[mHor][mVer]=" X ";
             }
         }
+        
+        System.out.println("-----------------------------");
+        System.out.println("| "+mArreglo[0][0]+" | "+mArreglo[0][1]+" | "+mArreglo[0][2]+" |");
+        System.out.println("-----------------------------");
+        System.out.println("| "+mArreglo[1][0]+" | "+mArreglo[1][1]+" | "+mArreglo[1][2]+" |");
+        System.out.println("-----------------------------");
+        System.out.println("| "+mArreglo[2][0]+" | "+mArreglo[2][1]+" | "+mArreglo[2][2]+" |");
+        System.out.println("-----------------------------");
         
         
     }
